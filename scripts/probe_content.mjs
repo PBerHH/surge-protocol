b536f57000821d65991573a818ff35c9c7c2816c
surge-dev <hhatberlin@gmail.com>
2026-09-08 09:40:41 +0200

    chore: remove debug/probe scripts from RPC migration

--- scripts/probe_content.mjs
+++
@@ -1,6 +0,0 @@
-import { SuiGrpcClient } from '@mysten/sui/grpc';
-const VAULT = '0xcc6a5e55e3099b2b9d777b9f51b6a5807a03888c613be0b401468a94cc3f1ba5';
-const client = new SuiGrpcClient({ network: 'mainnet', baseUrl: 'https://fullnode.mainnet.sui.io:443' });
-const v = await client.getObject({ objectId: VAULT });
-const util = await import('node:util');
-console.log(util.inspect(v.object, { depth: 6, maxArrayLength: 20 }));
--- scripts/probe_json.mjs
+++
@@ -1,5 +0,0 @@
-import { SuiGrpcClient } from '@mysten/sui/grpc';
-const VAULT = '0xcc6a5e55e3099b2b9d777b9f51b6a5807a03888c613be0b401468a94cc3f1ba5';
-const client = new SuiGrpcClient({ network: 'mainnet', baseUrl: 'https://fullnode.mainnet.sui.io:443' });
-const v = await client.getObject({ objectId: VAULT });
-console.log(JSON.stringify(v.object.json, (k, val) => typeof val === 'bigint' ? val.toString() : val, 2));
